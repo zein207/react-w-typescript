5deb10e02bdd7529359fda395d3c78a8ff5dc611
David <zein207@gmail.com>
2022-05-16 12:14:30 -0500

    Forms

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@
 // import { Counter } from "./components/Counter";
 // import { CounterWithHook } from './components/CounterWithHook';
 // import { Login } from './components/Login';
+// import { Users } from "./components/Users";
 
-import { Users } from "./components/Users";
-
+import { Forms } from "./components/Forms";
 
 const App = () => {
 
@@ -20,7 +20,8 @@ const App = () => {
       {/* <Counter /> */}
       {/* <CounterWithHook /> */}
       {/* <Login /> */}
-      <Users />
+      {/* <Users /> */}
+      <Forms />
     </div>
   )
 }
